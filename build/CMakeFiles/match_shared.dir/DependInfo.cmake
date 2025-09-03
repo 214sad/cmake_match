d4ad81bfafe4fc661bbdf43487c0d6a1b25644e7
Tom <21462358@qq.com>
2025-09-03 16:27:27 +0800

    success use shared

---
+++ build/CMakeFiles/match_shared.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/Tom/Cmake/test_Cmake/src/manage.cpp" "CMakeFiles/match_shared.dir/src/manage.cpp.o" "gcc" "CMakeFiles/match_shared.dir/src/manage.cpp.o.d"
+  "/home/Tom/Cmake/test_Cmake/src/student.cpp" "CMakeFiles/match_shared.dir/src/student.cpp.o" "gcc" "CMakeFiles/match_shared.dir/src/student.cpp.o.d"
+  "/home/Tom/Cmake/test_Cmake/src/teacher.cpp" "CMakeFiles/match_shared.dir/src/teacher.cpp.o" "gcc" "CMakeFiles/match_shared.dir/src/teacher.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/match_shared.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/match_shared.dir/src/manage.cpp.o"
+  "CMakeFiles/match_shared.dir/src/manage.cpp.o.d"
+  "CMakeFiles/match_shared.dir/src/student.cpp.o"
+  "CMakeFiles/match_shared.dir/src/student.cpp.o.d"
+  "CMakeFiles/match_shared.dir/src/teacher.cpp.o"
+  "CMakeFiles/match_shared.dir/src/teacher.cpp.o.d"
+  "libmatch_shared.pdb"
+  "libmatch_shared.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/match_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
